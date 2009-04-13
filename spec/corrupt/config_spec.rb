3c4c195287468e8a56d083142512b092af83180b
Dale Campbell <dale@save-state.net>
2009-04-13 03:58:42 -0500

    doh

--- spec/corrupt/config_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe Config do
-  it 'should have a name' do
-    Corrupt::Config['name'].should_not be_nil
-  end
-end
--- spec/corrupt/system_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe System do
-  it 'should boot and load the config' do
-    System.boot!
-    Corrupt::Config.should_not be_nil
-  end
-end
