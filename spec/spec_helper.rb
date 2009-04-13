e004639c477effe625133e63c05f568d9c5dcd20
Dale Campbell <dale@save-state.net>
2009-04-12 23:16:47 -0500

    fixing spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+require 'rubygems'
 require 'spec'
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/corrupt')
 
+ENV['CORRUPT_ENV'] = 'test'
+Corrupt.boot!
+
 Spec::Runner.configure do |config|
+  config.before(:all) do
+    DataMapper.auto_migrate!
+  end
 end
